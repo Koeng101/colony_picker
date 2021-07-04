9c3533c4e32deb35ea30a2c380f6ab3b6280843d
Max Schommer <maxschommer@gmail.com>
2021-07-03 17:56:39 -0700

    Initial commit

---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="colony_picker",  # Replace with your own username
+    version="0.0.1",
+    description="Adding colony picking ability to the AR3 robot arm.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Koeng101/colony_picker",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'numpy',
+        'numpy-quaternion',
+        'opencv-python'
+    ],
+    extras_require={
+
+    },
+    python_requires='>=3.6',
+)
