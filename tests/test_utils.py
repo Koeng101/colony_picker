a9d582261e0a269feb77bfdcee8a288b9897aca8
Max Schommer <maxschommer@gmail.com>
2021-07-10 16:24:53 -0700

    Fiducial fuser works, still documenting

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,12 +1,19 @@
 import unittest
 import cv2
-from colony_picker.utils import generate_fiducials
+from colony_picker.utils import generate_fiducials, calilbrate_camera
 
 
 class TestUtils(unittest.TestCase):
     def test_generate_fiducials(self):
         tag_dict = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_6X6_250)
         generate_fiducials(tag_dict, list(range(12)), 32, "test_tags", True)
+
+    def test_calibrate_camera(self):
+        square_size = 19.3  # millimeters
+        x_squares = 9
+        y_squares = 6
+        vid_file = ""
+        calilbrate_camera()
 
 
 if __name__ == "__main__":
