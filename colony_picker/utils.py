d936e239f85e6ff13a4a8256e19d96b996422ec0
Max Schommer <maxschommer@gmail.com>
2021-07-18 13:34:24 -0700

    Added camera calibration function to utils

--- colony_picker/utils.py
+++ colony_picker/utils.py
@@ -1,6 +1,7 @@
 from typing import Any, Optional
 import textwrap
 from scipy.spatial.transform import Rotation
+import random
 import cv2
 import numpy as np
 
@@ -238,3 +239,83 @@ def rotate_image_card_3d(
     res = cv2.warpPerspective(
         img, persp_mat, (*out_size,), borderValue=255)
     return res
+
+
+def calibrate_camera(board_squares_x: int, board_squares_y: int,
+                     board_square_size: float, frames: np.ndarray,
+                     num_samples: int = 20):
+    """Calibrate a camera using a know chess board and a sequence of images
+    represented by a numpy array. If more images are present in the array than
+    num_samples, then the images are sampled randomly to solve for the
+    camera calibration, so the result may be non-deterministic.
+
+    Args:
+        board_squares_x (int): The number of squares on the chess board in the
+            x direction. To count the squares, count both black and white
+            squares.
+        board_squares_y (int): The number of squares on the chess board in the
+            y direction. To count the squares, count both black and white
+            squares.
+        board_square_size (float): The size of the squares on the physical
+            board (in the units of your choice). The units you choose will
+            affect the calibration matrices returned.
+        frames (np.ndarray): An #Nx#Mx#O array of grayscale frames. There are #N
+            frames, and the width and height are #O and #M respectively.
+        num_samples (int, optional): The number of samples to take from the
+            frames (which have detected the chessboard validly). Defaults to 20.
+
+    Raises:
+        ValueError: Frames must be grayscale (last dimension must not be 3)
+        ValueError: Wrong number of dimensions detected.
+        RuntimeError: No chessboard corners were detected.
+
+    Returns:
+        Tuple[np.ndarray, np.ndarray]: A Tuple comprising:
+            (np.ndarray): A 3x3 camera matrix
+            (np.ndarray): A vector of length 5 of distortion coefficients. (k1,
+                k2, p1, p2, k3)
+    """
+    board_points_x = board_squares_x - 1
+    board_points_y = board_squares_y - 1
+
+    termination_criteria = (cv2.TERM_CRITERIA_EPS +
+                            cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+
+    object_points = np.zeros(
+        (board_points_x * board_points_y, 3), dtype=np.float32)
+    object_points[:, :2] = np.mgrid[0:board_points_x,
+                                    0:board_points_y].T.reshape(-1, 2) * board_square_size
+
+    if frames.shape[-1] == 3:
+        raise ValueError("Frames must be converted to gray.")
+    if frames.ndim != 3:
+        raise ValueError(f"Frames have {frames.ndim} dimensions. Expected 3.")
+
+    all_corners = []
+    for i, frame in enumerate(frames):
+        ret, corners = cv2.findChessboardCorners(
+            frame, (board_points_x, board_points_y),
+            cv2.CALIB_CB_ADAPTIVE_THRESH + cv2.CALIB_CB_FAST_CHECK +
+            cv2.CALIB_CB_NORMALIZE_IMAGE)
+        if ret:
+            corners_refined = cv2.cornerSubPix(frame, corners,
+                                               (11, 11), (-1, -1),
+                                               termination_criteria)
+
+            all_corners.append(corners_refined.reshape(-1, 2))
+
+    if len(all_corners) == 0:
+        raise RuntimeError("No corners were detected. The board_squares_x or y"
+                           " may be incorrect, or the full chessboard might not be in"
+                           " focus or in view for any frames.")
+
+    if len(all_corners) > num_samples:
+        samp_idxs = random.sample(range(len(all_corners)), num_samples)
+        all_corners = [all_corners[i] for i in samp_idxs]
+    object_points = [object_points] * len(all_corners)
+
+    ret, cam_mat, dist, _, _ = cv2.calibrateCamera(object_points,
+                                                   all_corners,
+                                                   frames.shape[1::-1], None,
+                                                   None)
+    return cam_mat, np.squeeze(dist)
