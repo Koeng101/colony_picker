16a88d16ba9d390a925bc835543855ec5e763a66
ramininaieni <ramininaieni@hmc.edu>
2021-08-17 23:26:56 -0700

    packaging DH parameters into a variable rather than global constant

--- colony_picker/numerical_inverse_kinematics.py
+++ colony_picker/numerical_inverse_kinematics.py
@@ -8,22 +8,12 @@ from scipy.spatial.transform import Rotation
 from functools import partial
 import logging
 
-# Denavit-Hartenberg Parameters of AR3 provided by
-# AR2 Version 2.0 software executable files from
-# https://www.anninrobotics.com/downloads
-# parameters are the same between the AR2 and AR3
-alphas = [-(math.pi/2), 0, math.pi/2, -(math.pi/2), math.pi/2, 0]
-a_vals = [64.2, 305, 0, 0, 0, 0]
-d_vals = [169.77, 0, 0, -222.63, 0, -36.25]
-theta_offsets = [0, 0, -math.pi/2, 0, 0, math.pi]
-
-NUM_JOINTS = 6
-
 # Got this formula for the transformation matrices using DH parameters
 # from this site: https://blog.robotiq.com/how-to-calculate-a-robots-forward-kinematics-in-5-easy-steps
 
 
-def get_t_mat(theta, theta_offset, a, alpha, d):
+def get_t_mat(theta, single_joint_dh_param):
+    theta_offset, alpha, a, d = single_joint_dh_param
     theta += theta_offset
     return np.array([[math.cos(theta),
                       -math.sin(theta)*math.cos(alpha),
@@ -37,15 +27,12 @@ def get_t_mat(theta, theta_offset, a, alpha, d):
                      [0, 0, 0, 1]])
 
 
-def get_t_mats(thetas):
+def get_t_mats(thetas, dh_params):
+    num_joints = len(dh_params[0])
     t_mats = []
     accumulator_t_mat = np.identity(4)
-    for joint_idx in range(NUM_JOINTS):
-        t_mat = get_t_mat(thetas[joint_idx],
-                          theta_offsets[joint_idx],
-                          a_vals[joint_idx],
-                          alphas[joint_idx],
-                          d_vals[joint_idx])
+    for joint_idx in range(num_joints):
+        t_mat = get_t_mat(thetas[joint_idx], dh_params[:, joint_idx])
         accumulator_t_mat = np.matmul(accumulator_t_mat, t_mat)
         t_mats.append(accumulator_t_mat)
 
@@ -110,8 +97,8 @@ def convert_pose_to_euler(pose_in_quat):
     return pose_in_euler
 
 
-def get_end_effector_pose(thetas, euler=False):
-    t_mats = get_t_mats(thetas)
+def get_end_effector_pose(thetas, dh_params, euler=False):
+    t_mats = get_t_mats(thetas, dh_params)
     end_effector_position = t_mats[-1][:3, 3]
     rot_mat = t_mats[-1][:3, :3]
     if euler:
@@ -146,38 +133,41 @@ def get_mean_squared_error(error_vector):
     return error
 
 
-def objective_function(thetas, desired_end_effector_pose):
-    current_end_effector_pose = get_end_effector_pose(thetas)
+def objective_function(thetas, desired_end_effector_pose, dh_params):
+    current_end_effector_pose = get_end_effector_pose(
+        thetas, dh_params)
     error_vector = get_error_vector(
         desired_end_effector_pose, current_end_effector_pose)
     return get_mean_squared_error(error_vector)
 
 
-def scipy_find_joint_angles(thetas_init, desired_end_effector_pose):
+def scipy_find_joint_angles(thetas_init, desired_end_effector_pose, dh_params):
     result = minimize(objective_function, thetas_init,
-                      (desired_end_effector_pose,), method='BFGS')
+                      (desired_end_effector_pose, dh_params,), method='BFGS')
     if result.fun > 1e-07:
-        print("ERROR TOO HIGH")
-    print(result)
+        print("error greater than 1e-07")
     return (result.x, result.fun)
 
 
-def animate_inverse_kinematics_sliders():
-    joint_positions = np.zeros((NUM_JOINTS + 1, 3))
+def animate_inverse_kinematics_sliders(dh_params, position_bound=700):
+    num_joints = len(dh_params[0])
+    joint_positions = np.zeros((num_joints + 1, 3))
     p = pv.Plotter()
     draw_coordinate_system(p, np.eye(4), base=True)
     thetas_init = np.zeros(6)
-    desired_end_effector_pose = get_end_effector_pose(thetas_init, euler=True)
+    desired_end_effector_pose = get_end_effector_pose(
+        thetas_init, dh_params, euler=True)
 
     def callback(idx, pose_param):
         desired_end_effector_pose[idx] = pose_param
         desired_end_effector_pose_quat = convert_pose_to_quat(
             desired_end_effector_pose)
         thetas, error = scipy_find_joint_angles(
-            thetas_init, desired_end_effector_pose_quat)
+            thetas_init, desired_end_effector_pose_quat, dh_params)
         if error < 0.1:
             thetas_init[:] = thetas
-        t_mats = get_t_mats(thetas)
+        t_mats = get_t_mats(thetas, theta_offsets,
+                            alpha_vals, a_vals, d_vals, num_joints)
         for i, t_mat in enumerate(t_mats):
             joint_positions[i + 1, :3] = t_mat[:3, 3]
             draw_coordinate_system(p, t_mat, name=f"theta{i}")
@@ -185,9 +175,7 @@ def animate_inverse_kinematics_sliders():
     end_effector_pose_labels = ["x", "y", "z", "x_rot", "y_rot", "z_rot"]
     for i in range(6):
         if i <= 2:
-            # TODO(ramininaieni): make the upper and lower bounds for position
-            # more concrete
-            lower_bound = -700
+            lower_bound = -position_bound
             upper_bound = -lower_bound
         else:
             lower_bound = -np.pi
@@ -201,23 +189,26 @@ def animate_inverse_kinematics_sliders():
     p.show()
 
 
-def animate_inverse_kinematics_sphere():
-    joint_positions = np.zeros((NUM_JOINTS + 1, 3))
+def animate_inverse_kinematics_sphere(dh_params):
+    num_joints = len(dh_params[0])
+    joint_positions = np.zeros((num_joints + 1, 3))
     p = pv.Plotter()
     draw_coordinate_system(p, np.eye(4), base=True)
     thetas_init = np.zeros(6)
-    desired_end_effector_pose = get_end_effector_pose(thetas_init, euler=True)
+    desired_end_effector_pose = get_end_effector_pose(
+        thetas_init, dh_params, euler=True)
 
     def callback(idx, pose_param):
         desired_end_effector_pose[idx] = pose_param
         desired_end_effector_pose_in_quat = convert_pose_to_quat(
             desired_end_effector_pose)
         thetas, error = scipy_find_joint_angles(
-            thetas_init, desired_end_effector_pose_in_quat)
+            thetas_init, desired_end_effector_pose_in_quat, dh_params)
         if error < 0.1:
             thetas_init[:] = thetas
 
-        t_mats = get_t_mats(thetas)
+        t_mats = get_t_mats(thetas, theta_offsets,
+                            alpha_vals, a_vals, d_vals, num_joints)
         for i, t_mat in enumerate(t_mats):
             joint_positions[i + 1, :3] = t_mat[:3, 3]
             draw_coordinate_system(p, t_mat, name=f"theta{i}")
@@ -236,21 +227,22 @@ def animate_inverse_kinematics_sphere():
     p.show()
 
 
-def animate_forward_kinematics():
-    joint_positions = np.zeros((NUM_JOINTS + 1, 3))
+def animate_forward_kinematics(dh_params):
+    num_joints = len(dh_params[0])
+    joint_positions = np.zeros((num_joints + 1, 3))
     p = pv.Plotter()
     draw_coordinate_system(p, np.eye(4), True)
     thetas = np.zeros(6)
 
     def callback(idx, theta):
         thetas[idx] = theta
-        t_mats = get_t_mats(thetas)
+        t_mats = get_t_mats(thetas, dh_params)
         for i, t_mat in enumerate(t_mats):
             joint_positions[i + 1, :3] = t_mat[:3, 3]
             draw_coordinate_system(p, t_mat, name=f"theta_{i}")
         draw_links(p, joint_positions)
 
-    for i in range(NUM_JOINTS):
+    for i in range(num_joints):
         p.add_slider_widget(partial(callback, (i,)),
                             [-np.pi, np.pi], pointa=(0.7, 0.9-0.15*i),
                             pointb=(0.95, 0.9-0.15*i),
@@ -259,6 +251,17 @@ def animate_forward_kinematics():
 
 
 if __name__ == "__main__":
-    # animate_inverse_kinematics_sliders()
-    animate_inverse_kinematics_sphere()
-    # animate_forward_kinematics()
+    # Denavit-Hartenberg Parameters of AR3 provided by
+    # AR2 Version 2.0 software executable files from
+    # https://www.anninrobotics.com/downloads
+    # parameters are the same between the AR2 and AR3
+    alpha_vals = [-(math.pi/2), 0, math.pi/2, -(math.pi/2), math.pi/2, 0]
+    a_vals = [64.2, 305, 0, 0, 0, 0]
+    d_vals = [169.77, 0, 0, -222.63, 0, -36.25]
+    theta_offsets = [0, 0, -math.pi/2, 0, 0, math.pi]
+
+    dh_params = np.array([theta_offsets, alpha_vals, a_vals, d_vals])
+
+    # animate_inverse_kinematics_sliders(dh_params)
+    # animate_inverse_kinematics_sphere(dh_params)
+    animate_forward_kinematics(dh_params)
